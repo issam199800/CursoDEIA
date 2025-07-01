efb1de99ef9e819f66d2e85548ee9d9f155ecd64
IssamLahmani <hichamlahmaniaghan@gmail.com>
2025-07-01 12:07:22 +0200

    Nuevos commit

---
+++ Conceptosbásicos.py/Ejercicio_entrada.py
@@ -0,0 +1,21 @@
+print('Introducir valor a:')
+valor_a = input()
+
+print('Introducir el valor b:')
+valor_b = input()
+
+print(valor_a)
+print(valor_b)
+
+resultado =int(valor_a) + int(valor_b)
+
+print(resultado)
+
+#Tenemos quer almacenra ese valor de una varibale
+
+#El resultado es 5060 porque los valores de valor_s y valor_b los está tomando como texto.
+
+#Vamos a convertir los valores en número enteros
+#Para convertir estos valores en números enteros sólo hay que poner la función int()
+
+
